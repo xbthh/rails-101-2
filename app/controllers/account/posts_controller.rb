8dc45b88011c0d629023b497e94bad1b6f0754cd
xbthh <hanhan_smith@hotmail.com>
2016-10-07 22:59:34 +0800

    add account/edit.html,but can't complet

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,23 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+
+  def new
+    @group = Group.find(params[:group_id])
+    @post = Post.new
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.destroy
+    flash[:alert] = "Group deleted"
+    redirect_to groups_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
 end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -73,7 +73,6 @@ def join
 
   def find_group_and_check_permission
     @group = Group.find(params[:id])
-            Group.find(params[:id])
     if current_user != @group.user
       redirect_to root_path,alert:"You have no permission."
     end
